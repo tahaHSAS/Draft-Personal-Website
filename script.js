8584cef6185da01acd0703d7ac1a4f15cf7de65b
TahaMridha <>
2021-08-02 14:21:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function thanks(){
+          document.getElementById("form").innerHTML = "Thank You!"
+ }
+function showGundam(){
+          document.getElementById("one").innerHTML = "This anime has very good animation!";
+ }
+function tkh() {
+          document.getElementById("project1").innerHTML = "I'm learning how to make my own website with TKH!";
+ }
+
+
+ /* My 3 different event listeners were click, onmouseover. and doubleclick
+ 
+ I implemented these in interests, contacts, and projects.
+ 
+ 
+ */  
